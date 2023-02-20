c4e74e4f3ab852d42961e21a1b9f5b0f04ba5b67
Slava Nazimko <vnazimko@gmail.com>
2023-02-20 15:04:32 +0300

    feat: added colors

---
+++ FashionStore/Extentions/String+setStyle.swift
@@ -0,0 +1,26 @@
+//
+//  String+setStyle.swift
+//  FashionStore
+//
+//  Created by Vyacheslav on 19.02.2023.
+//
+
+//import UIKit
+//
+//struct TextStyle {
+//    var font: UIFont
+//    var size: Int
+//    var color: UIColor
+//    var lineHeight: Int
+//    var letterSpacing: Int
+//}
+//
+//extension TextStyle {
+//    static let titleL = TextStyle(
+//        font: <#T##UIFont#>,
+//        size: 18,
+//        color: <#T##UIColor#>,
+//        lineHeight: <#T##Int#>,
+//        letterSpacing: <#T##Int#>
+//    )
+//}
