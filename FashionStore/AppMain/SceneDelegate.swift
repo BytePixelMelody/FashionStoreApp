36cdfd42de58a798b66a7eec1d60b9e9dd905af1
Slava Nazimko <vnazimko@gmail.com>
2023-03-07 18:59:17 +0300

    feat: added manual launch from SceneDelegate

--- FashionStore/AppMain/SceneDelegate.swift
+++ FashionStore/AppMain/SceneDelegate.swift
@@ -13,10 +13,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        let rootNavigationController = UINavigationController()
+        rootNavigationController.navigationBar.isHidden = true
+        
+        let moduleBuilder = ModuleBuilder()
+        let router = Router(navigationController: rootNavigationController, moduleBuilder: moduleBuilder)
+        router.showTestScreen()
+        
+        window.rootViewController = rootNavigationController
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
