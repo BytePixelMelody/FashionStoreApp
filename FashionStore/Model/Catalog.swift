7c3d666cdb81a7fa561d762b0806d9203451821e
Slava Nazimko <vnazimko@gmail.com>
2023-05-21 11:12:48 +0300

    fix: edited Model structure and catalog.json

--- FashionStore/Model/Catalog.swift
+++ FashionStore/Model/Catalog.swift
@@ -31,7 +31,7 @@ struct Model: Codable {
     var price: Decimal // 45.00...950.00
     var images: [String] // ["100001", "200001", "300001" etc]
     var material: String // "Cotton", "Wool", "Linum" etc
-    var description: String // "Long sleeve jacket with tuxedo collar. Front flap pockets and chest welt pocket. Tonal matching inner lining. Front button closure." etc
+    var modelDescription: String // "Long sleeve jacket with tuxedo collar. Front flap pockets and chest welt pocket. Tonal matching inner lining. Front button closure." etc
     var styles: [Style]
     var colors: [Color]
 }
